3e0962fb028f3d73f86ec720223e5e73efc63f41
Ivan <brandy.bg@mail.ru>
2018-11-29 16:26:13 +0300

    Без абстрактных классов

---
+++ Kalkulator/Kalkulator/InitializerMatrix.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    
+
+    internal class InitializerMatrix
+    {
+        static int row;
+        static int column;
+        static Matrix matrixOne;
+        static Matrix matrixTwo;
+
+        private static void InitializerVariables()
+        {
+            column = SetVariableSize("колонок");
+            row = SetVariableSize("строк");
+        }
+        
+        public static int SetVariableSize(string variableName)
+        {
+            int variable = 0;
+            Console.Write($"Введите количество {variableName}: ");
+            variable = int.Parse(Console.ReadLine());
+            return variable;
+        }
+
+        private static Matrix CreateMatrix()
+        {
+            return new Matrix(column, row);
+        }
+
+        public static Matrix AddMatrix()
+        {
+            InitializerVariables();
+            Matrix matrix = CreateMatrix();
+            Console.WriteLine("Задайте значения матрицы:");           
+            for(int i = 0; i < matrix.Value.Length; i++)
+            {                
+                for(int j = 0; j < matrix.Value[i].Length; j++)
+                {
+                    Console.Write("Число: ");                    
+                    matrix.Value[i][j] = decimal.Parse(Console.ReadLine());
+                }
+            }                                              
+            return matrix;
+        }
+        
+        public static void AdditionTwoMatrix()
+        {
+            matrixOne = AddMatrix();
+            Console.WriteLine("Матрица 1: ");
+            Matrix.ShowMatrix(matrixOne);
+            matrixTwo = AddMatrix();
+            Console.WriteLine("Матрица 2: ");
+            Matrix.ShowMatrix(matrixTwo);
+        }
+
+
+
+        public static void Choice()
+        {
+            Console.Write("Введите действие: ");
+            string selection = Console.ReadLine();
+            switch (selection)
+            {                
+                case "+":
+                    AdditionTwoMatrix();
+                    Matrix matrixSum = matrixOne + matrixTwo;
+                    Console.WriteLine("Сумма первой и второй матрицы: ");
+                    Matrix.ShowMatrix(matrixSum);
+                    break;
+
+                case "*":
+                    AdditionTwoMatrix();
+                    Matrix matrixMult = matrixOne * matrixTwo;
+                    Console.WriteLine("Умножение первой и второй матрицы равно: ");
+                    Matrix.ShowMatrix(matrixMult);
+                    break;
+
+                case "-":
+                    AdditionTwoMatrix();
+                    Matrix matrixDifference = matrixOne - matrixTwo;
+                    Console.WriteLine("Разность первой и второй матрицы: ");
+                    Matrix.ShowMatrix(matrixDifference);
+                    break;
+
+                case "/":
+                    AdditionTwoMatrix();
+                    Matrix matrixShare = matrixOne / matrixTwo;
+                    Console.WriteLine("Деление первой и второй матрицы: ");
+                    Matrix.ShowMatrix(matrixShare);
+                    break;
+
+                case "T":
+                    matrixOne = AddMatrix();
+                    Console.WriteLine("Матрица: ");
+                    Matrix.ShowMatrix(matrixOne);
+                    Console.WriteLine("Транспорированая матрица");
+                    Matrix matrixTrans = matrixOne.Transport();
+                    Matrix.ShowMatrix(matrixTrans);
+                    break;
+                default:
+                    Console.WriteLine("Не корректный ввод");
+                    break;
+
+
+            }
+        }
+
+
+    }
+}
---
+++ Kalkulator/Kalkulator/Matrix.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator
+{
+    internal class Matrix
+    {
+        public int CountOfColumn { get; }
+        public int CountOfRow { get; }
+
+
+        public decimal[][] Value;
+
+
+
+        public Matrix(int countOfColumn, int countOfRow)
+        {
+            CountOfColumn = countOfColumn;
+            CountOfRow = countOfRow;
+            Value = new decimal[CountOfRow][];
+
+            for (int i = 0; i < Value.Length; i++)
+            {
+                Value[i] = new decimal[CountOfColumn];
+            }
+        }
+
+        public decimal[] GetRow(int rowNumber)
+        {
+            return Value[rowNumber];
+        }
+
+        public decimal[] GetColumn(int columnNumber)
+        {
+            var result = new decimal[CountOfRow];
+            for (int i = 0; i < CountOfRow; i++)
+            {
+                result[i] = GetRow(i)[columnNumber];
+            }
+            return result;
+
+        }
+        
+        public static Matrix operator *(Matrix value1, Matrix value2)
+        {
+            if (value1.CountOfRow == value2.CountOfColumn)
+            {
+                var result = new Matrix(value1.CountOfRow, value1.CountOfRow);
+
+                for (int i = 0; i < value1.CountOfRow; i++)
+                {
+                    for (int j = 0; j < value1.CountOfRow; j++)
+                    {
+                        var column = value2.GetColumn(i);
+                        var row = value1.GetRow(j);
+                        result.Value[j][i] = MultVector(column, row);
+                    }
+                    
+                }
+                return result;
+            }
+
+            Console.WriteLine("Невозможно умножить матрицы");
+            return null;
+
+        }
+        
+        private static decimal MultVector(decimal[] value1, decimal[] value2)
+        {
+            decimal result = 0;
+            string res = "";
+            for (int i = 0; i < value1.Length; i++)
+            {
+                result += value1[i] * value2[i];
+                res += value1[i] + "*" + value2[i] + "+";
+            }
+
+            res = res.Substring(0, res.Length - 1) + "=" + result;
+
+            Console.WriteLine(res);
+
+            return result;
+        }
+
+        public static Matrix operator +(Matrix value1, Matrix value2)
+        {
+            if (value1.CountOfRow == value2.CountOfColumn)
+            {
+                var result = new Matrix(value1.CountOfColumn, value2.CountOfRow);
+                for (int i = 0; i < value1.CountOfColumn; i++)
+                {
+                    for (int j = 0; j < value1.CountOfRow; j++)
+                    {
+                        result.Value[i][j] = value1.Value[i][j] + value2.Value[i][j];
+                    }
+                }
+                return result;
+            }
+            Console.WriteLine("Невозможно сложить матрицы");
+            return null;            
+        }
+
+        public static Matrix operator -(Matrix value1, Matrix value2)
+        {
+            if (value1.CountOfRow == value2.CountOfColumn)
+            {
+                var result = new Matrix(value1.CountOfColumn, value2.CountOfRow);
+                for (int i = 0; i < value1.CountOfColumn; i++)
+                {
+                    for (int j = 0; j < value1.CountOfRow; j++)
+                    {
+                        result.Value[i][j] = value1.Value[i][j] - value2.Value[i][j];
+                    }
+                }
+                return result;
+            }
+
+            Console.WriteLine("Невызможно вычесть матрицы");
+            return null;
+        }
+
+        public static Matrix operator /(Matrix value1, Matrix value2)
+        {
+            if(value1.CountOfRow == value2.CountOfColumn)
+            {
+                var result = new Matrix(value1.CountOfRow, value1.CountOfRow);
+
+                for (int i = 0; i < value1.CountOfRow; i++)
+                {
+                    for (int j = 0; j < value1.CountOfRow; j++)
+                    {
+                        var column = value2.GetColumn(i);
+                        var row = value1.GetRow(j);
+                        result.Value[i][j] = ShareMatrix(column, row);
+                    }
+                }
+                return result;
+            }
+            Console.WriteLine("Невозможно раздерить матрицы");
+            return null;
+        }
+
+        private static decimal ShareMatrix(decimal[] value1, decimal[] value2)
+        {
+            decimal result = 0;
+            string res = "";
+            for (int i = 0; i < value1.Length; i++)
+            {
+                result += value1[i] / value2[i];
+                res += value1[i] + "/" + value2[i] + "+";
+
+            }
+            res = res.Substring(0, res.Length - 1) + "=" + result;
+
+            Console.WriteLine(res);
+            return result;
+        }
+
+        public Matrix Transport()
+        {
+            var transMatrix = new Matrix(CountOfColumn, CountOfRow);
+            for(int i = 0; i < CountOfRow; i++)
+            {
+                for(int j = 0; j < CountOfColumn; j++)
+                {
+                    transMatrix.Value[j][i] = Value[i][j];
+                } 
+            }
+            return transMatrix;
+        }
+
+
+        
+
+        public static void ShowMatrix(Matrix matrix)
+        {
+          for(int i =0; i < matrix.Value.Length; i++)
+            {
+                for(int j = 0; j < matrix.Value[i].Length; j++)
+                {
+                    Console.Write(String.Format("{0:F1}", matrix.Value[i][j]) + "\t");
+                }
+                Console.WriteLine();
+            }   
+        }
+    }
+}
